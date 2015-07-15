24eca9e3b5310345f48d5662865f753c37654ebb
KORIAS <Atsuki@kanekoatsuki-no-MacBook-Air.local>
2015-07-15 15:57:58 +0900

    first

---
+++ main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  課題01A
+//
+//  Created by 金子敦樹 on 2015/05/29.
+//  Copyright (c) 2015年 金子敦樹. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("電子");         //printfは表示
+    printf("太郎");
+    
+    printf("電子\n");       //\nは改行（windowsでは¥n）
+    printf("太郎");
+    
+    return 0;
+}
